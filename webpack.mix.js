be3a7be8f75f51fe8ef0f8693f3d856dd89dc728
pgm-zegerschaeverbeke <zeger.schaeverbeke@student.arteveldehs.be>
2025-10-01 17:52:15 +0200

    Remove unused postcss-for dependency from package.json and webpack.mix.js

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,7 +2,6 @@ let mix = require("laravel-mix");
 
 mix.js("src/scripts/app.js", "build/scripts").setPublicPath("build");
 mix.postCss("src/styles/main.css", "build/styles", [
-  require("postcss-for"),
   require("postcss-custom-properties")
 ]).options({
   processCssUrls: false
